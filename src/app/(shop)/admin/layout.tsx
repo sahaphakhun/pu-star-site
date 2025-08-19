7dbe36d91f256d6946f5238705863d271c92b376
aaergaegdf <67260413+sahaphakhun@users.noreply.github.com>
2025-08-19 15:31:47 +0700

    fix: ensure admin sidebar remains clickable

--- src/app/(shop)/admin/layout.tsx
+++ src/app/(shop)/admin/layout.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 const AdminLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen flex bg-gray-50">
+    <div className="min-h-screen bg-gray-50">
       <Toaster position="top-right" />
       <AdminSidebar />
-      <main className="flex-1 pt-16 md:pt-8 p-4 md:p-8 overflow-x-auto">{children}</main>
+      <main className="pt-16 md:pt-8 p-4 md:p-8 md:ml-56 overflow-x-auto">{children}</main>
     </div>
   );
 };
