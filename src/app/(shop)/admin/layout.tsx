2f73ffcf462d7ae1d527ce8f34ab5b586a9289e9
aaergaegdf <67260413+sahaphakhun@users.noreply.github.com>
2025-08-19 17:25:39 +0700

    Fix admin layout width calculation

--- src/app/(shop)/admin/layout.tsx
+++ src/app/(shop)/admin/layout.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 const AdminLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 overflow-x-hidden">
       <Toaster position="top-right" />
       <AdminSidebar />
-      <main className="pt-16 md:pt-8 p-4 md:p-8 md:ml-56 overflow-x-auto">{children}</main>
+      <main className="pt-16 md:pt-8 p-4 md:p-8 md:ml-56 md:w-[calc(100%_-_14rem)] overflow-x-auto">{children}</main>
     </div>
   );
 };
